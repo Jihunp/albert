863863390237cbab65cfd5b1da6e0e5c7669674e
jihunp <edwardpark.park@gmail.com>
2024-11-06 14:20:37 -0800

    crated a screenshot that shares image daata to backend

---
+++ app/channels/chat_channel.rb
@@ -0,0 +1,15 @@
+class ChatChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "chat_channel"
+  end
+
+  def receive(data)
+    image_data = data['image']
+    p image_data
+    p "success"
+    # later handles screenshot data
+  end
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
--- app/javascript/controllers/webcam_controller.js
+++ app/javascript/controllers/webcam_controller.js
@@ -1,10 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
+import { createConsumer } from "@rails/actioncable"
 
 export default class extends Controller {
   static targets = ["video", "canvas"]
 
   connect() {
     this.startWebcam()
+    this.createSubscription()
+
   }
 
   startWebcam() {
@@ -18,11 +21,22 @@ export default class extends Controller {
       })
   }
 
+  createSubscription() {
+    this.channel = createConsumer().subscriptions.create("ChatChannel", {
+      received: (data) => {
+        console.log("received data from backend", data)
+      }
+    })
+  }
+
   capture() {
     const canvas = this.canvasTarget
     const context = canvas.getContext("2d")
     canvas.width = this.videoTarget.videoWidth
     canvas.height = this.videoTarget.videoHeight
     context.drawImage(this.videoTarget, 0, 0, canvas.width, canvas.height)
+
+    const imageData = canvas.toDataURL("image/png")
+    this.channel.send({ image: imageData })
   }
 }
